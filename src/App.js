09df6b05d70e3fa407936d02cd4c9160ef7e633b
Varun Hegde <varun.hegde@gatech.edu>
2022-04-07 16:51:14 -0400

    Github setup

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Priority Queue Test!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Testing one more time for Git!</p>
       </header>
     </div>
   );
